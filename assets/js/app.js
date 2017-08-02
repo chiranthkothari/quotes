8073f0446dc9de5a7510e3460580cdb1d63ff9b4
Chiranth Kothari <chiranthkothari96@gmail.com>
2017-08-02 08:02:30 +0530

    NA

---
+++ assets/js/app.js
@@ -0,0 +1,79 @@
+function quote(){
+    var randomNo= Math.floor(Math.random()*20);
+
+    switch (randomNo){
+        case 2:
+        $('.aq').text("Always look your best");
+        break;
+
+        case 3:
+        $('.aq').text("Fuck excuses, yot either want it or you dont");
+        break;
+
+        case 4:
+        $('.aq').text("The time is always now");
+        break;
+
+        case 5:
+        $('.aq').text("You cant stop someone who knows what they want");
+        break;
+
+        case 6:
+        $('.aq').text("Difficult doesnt mean impossible. It simply means you have to work hard");
+        break;
+
+        case 7:
+        $('.aq').text("The start is what stops most people");
+        break;
+
+        case 8:
+        $('.aq').text("Everyone wants to eat, but few are willing to hunt");
+        break;
+
+        case 9:
+        $('.aq').text("Do it with a passion or dont do it at all");
+        break;
+
+        case 10:
+        $('.aq').text("You'll never make history in comfort zone");
+        break;
+
+        case 11:
+        $('.aq').text("Stop Dreaming. Start Doing");
+        break;
+
+        case 12:
+        $('.aq').text("Patience, persistence & perspiration make an unbelievable combination for success");
+        break;
+
+        case 13:
+        $('.aq').text("Forget Netflix. Lets build an empire and chill");
+        break;
+
+        case 14:
+        $('.aq').text("Dont quit. Suffer now and live the rest of your life as champion");
+        break;
+
+        case 15:
+        $('.aq').text("Determination today leads to success tomorrow");
+        break;
+
+        case 16:
+        $('.aq').text("We all have 24hrs. Successful people use it, rest abuse it");
+        break;
+        
+        case 17:
+        $('.aq').text("Conquer your fears");
+        break;
+
+        case 18:
+        $('.aq').text("I see. I want. I grind.");
+        break;
+
+        case 19:
+        $('.aq').text("Live a life you dont need vacation from");
+        break;
+        
+
+    }
+}
